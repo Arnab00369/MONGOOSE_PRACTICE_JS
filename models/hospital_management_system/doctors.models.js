bce26ed91e82d915003efce46a411311d05c0bf8
Arnab Jana <arnab003jn.369@gmail.com>
2025-07-20 19:09:20 +0530

    changed done

--- models/hospital_management_system/doctors.models.js
+++ models/hospital_management_system/doctors.models.js
@@ -1,35 +1,7 @@
 import mongoose from 'mongoose';
 
 const doctorSchema = new mongoose.Schema({
-   name:{
-      type: String,
-      required: true,
-   },
-   diagnosedWith:{
-      type: String,
-      required: true
-   },
-   address:{
-      type: String,
-      required: true
-   },
-   age:{
-      type: Number,
-      required: true
-   },
-   bloodGroup:{
-      type: String,
-      required: true
-   },
-   gender:{
-      type: String,
-      enum: ["Male", "Female", "Others"],
-      required: true
-   },
-   admittedIn:{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Hospital",
-   }
+  
 },{timestamps: true});
 
 export const Doctor = mongoose.model('Doctor',doctorSchema);
